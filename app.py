8834b746bad4d9ba45c766f5b1060988e3a960fd
Philippe <philippe.mangin34@gmail.com>
2026-05-04 00:09:07 +0200

    Add export ical

--- app.py
+++ app.py
@@ -23,6 +23,24 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+    from flask import Response
+from datetime import datetime
+
+@app.route("/export-ical")
+def export_ical():
+    ical = """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Location Calendrier//FR
+BEGIN:VEVENT
+UID:1
+DTSTAMP:20240503T120000Z
+DTSTART:20240510T120000Z
+DTEND:20240515T120000Z
+SUMMARY:Réservation test
+END:VEVENT
+END:VCALENDAR"""
+    return Response(ical, mimetype="text/calendar")
+
 
 @app.route("/book", methods=["POST"])
 def book():
