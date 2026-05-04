2b3a76c1372f1b78b3206d237406c09778ce7c25
Philippe <philippe.mangin34@gmail.com>
2026-05-04 14:37:37 +0200

    Add Airbnb import route

--- app.py
+++ app.py
@@ -9,6 +9,26 @@ def home():
 
 @app.route("/export-ical")
 def export_ical():
+    import requests
+from icalendar import Calendar
+
+@app.route("/import-airbnb")
+def import_airbnb():
+    url = "https://www.airbnb.fr/calendar/ical/1408690958278432453.ics?t=a818b992302442ebbba047191301735f"
+    r = requests.get(url)
+    cal = Calendar.from_ical(r.text)
+
+    events = []
+    for component in cal.walk():
+        if component.name == "VEVENT":
+            events.append({
+                "start": component.get("DTSTART").dt,
+                "end": component.get("DTEND").dt,
+                "summary": str(component.get("SUMMARY"))
+            })
+
+    return {"reservations": events}
+
     ical = """BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Location Calendrier//FR
@@ -21,4 +41,5 @@ SUMMARY:Réservation test
 END:VEVENT
 END:VCALENDAR"""
     return Response(ical, mimetype="text/calendar")
+
 
