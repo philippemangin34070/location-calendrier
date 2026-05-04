33c90c93e558e4ee802d3578bd95111270a15e00
Philippe <philippe.mangin34@gmail.com>
2026-05-04 15:09:32 +0200

    Add dependencies

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response
-from datetime import datetime
+import requests
+from icalendar import Calendar
 
 app = Flask(__name__)
 
@@ -9,8 +10,18 @@ def home():
 
 @app.route("/export-ical")
 def export_ical():
-    import requests
-from icalendar import Calendar
+    ical = """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Location Calendrier//FR
+BEGIN:VEVENT
+UID:1
+DTSTAMP:20240503T120000Z
+DTSTART:20240510T120000Z
+DTEND:20240515T120000Z
+SUMMARY:Réservation test
+END:VEVENT
+END:VCALENDAR"""
+    return Response(ical, mimetype="text/calendar")
 
 @app.route("/import-airbnb")
 def import_airbnb():
@@ -28,18 +39,4 @@ def import_airbnb():
             })
 
     return {"reservations": events}
-
-    ical = """BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Location Calendrier//FR
-BEGIN:VEVENT
-UID:1
-DTSTAMP:20240503T120000Z
-DTSTART:20240510T120000Z
-DTEND:20240515T120000Z
-SUMMARY:Réservation test
-END:VEVENT
-END:VCALENDAR"""
-    return Response(ical, mimetype="text/calendar")
-
 
